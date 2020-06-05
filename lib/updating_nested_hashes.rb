d035788b2a1c3b21ff1e90ff3deaeaf6063fddb9
Eli Landau <eli.b.landau@gmail.com>
2020-06-05 17:57:39 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -29,8 +29,8 @@ def update_status
 
 
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
-  epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero]
+  epic_tragedy[:capulet][:heroine]
 end
 
 def add_characters
